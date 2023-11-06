a865a6504fcb12cd4bd6978527530e8dfc1be05e
Victoria Goltseva <katnisseverdeen191@mail.ru>
2023-11-06 23:23:40 +0300

    project init

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+
+    $('.multiple-items').slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        dotsClass: "my-dots",
+        autoplay: true
+    });
+
+});
